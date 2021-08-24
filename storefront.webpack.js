66d7813867772855bc1264c4edc804863c4ff9f4
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-08-24 20:30:53 -0300

    Update storefront.webpack.js

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -5,8 +5,8 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-     //'./html/ProductVariations.html': path.resolve(__dirname, 'template/js/html/ProductVariations.html'),
-     //'./js/ProductVariations.js': path.resolve(__dirname, 'template/js/ProductVariations.js')
+     './html/ProductVariations.html': path.resolve(__dirname, 'template/js/html/ProductVariations.html'),
+     './js/ProductVariations.js': path.resolve(__dirname, 'template/js/ProductVariations.js')
 
     }
   }
