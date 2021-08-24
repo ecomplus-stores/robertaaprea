ba34d2f3d469db60a69cb50d578dae2d2bad3dd6
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-08-24 20:26:30 -0300

    Create storefront.webpack.js

---
+++ storefront.webpack.js
@@ -0,0 +1,13 @@
+// storefront.webpack.js
+
+const path = require('path')
+
+module.exports = () => ({
+  resolve: {
+    alias: {
+     //'./html/ProductVariations.html': path.resolve(__dirname, 'template/js/html/ProductVariations.html'),
+     //'./js/ProductVariations.js': path.resolve(__dirname, 'template/js/ProductVariations.js')
+
+    }
+  }
+})
