80e8c3d8fb4dd87b02224142aa32aa2dbb1c55c6
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-09-10 20:28:34 -0300

    test: test of required

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -6,7 +6,8 @@ module.exports = () => ({
   resolve: {
     alias: {
      './html/ProductVariations.html': path.resolve(__dirname, 'template/js/html/ProductVariations.html'),
-     './js/ProductVariations.js': path.resolve(__dirname, 'template/js/ProductVariations.js')
+     './js/ProductVariations.js': path.resolve(__dirname, 'template/js/ProductVariations.js'),
+     './js/TheProduct.js': path.resolve(__dirname, 'template/js/TheProduct.js')
 
     }
   }
