cd5555fc7ef8473f65f71f1966af647f7bcac6af
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2021-09-15 23:27:01 -0300

    Update checkout.js

--- template/js/checkout.js
+++ template/js/checkout.js
@@ -1,2 +1,16 @@
 import '#template/js/checkout'
 import './custom-js/checkout'
+
+const routeArr = window.location.href.split('/')
+const route = routeArr[routeArr.length - 1]
+const route2 = routeArr[routeArr.length - 2]
+const route3 = routeArr[routeArr.length - 3]
+if(route == 'checkout') {
+  document.getElementById('checkToPay').style.display = 'none'
+}
+if(route2 == 'account') {
+    document.getElementById('checkToPay').style.display = 'none'
+}
+if(route3 == 'order') {
+    document.getElementById('checkToPay').style.display = 'none'
+}
