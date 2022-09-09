a0911f579b19b1a29e949179476186a66b2399f3
eduvlemes <edu.vlemes@gmail.com>
2022-09-09 12:13:28 -0300

    update alpix

--- template/js/custom-js/pages.js
+++ template/js/custom-js/pages.js
@@ -44,7 +44,8 @@ window.habilitarPresente = function(pid, _id){
     //console.log('buscou ' + pid);
     let customizations = JSON.parse(sessionStorage.getItem('gift_'+ pid ));
     //console.log(customizations);
-    let hasGiftWrap = customizations.find(el => ['460860165721941794800000','765390166152028331400000'].includes(el._id));
+    //let hasGiftWrap = customizations.find(el => ['460860165721941794800000','765390166152028331400000'].includes(el._id));
+    let hasGiftWrap = customizations.find(el => el.grid_id == "apx_gift_wrap");
     //console.log(hasGiftWrap);
     if(hasGiftWrap){
         $('[product_id="'+ _id +'"] .apx_giftWrap').removeClass('d-none');
